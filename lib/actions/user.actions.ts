9794b0d2c97782bc3ceb585814c28cdd88f7d438
Yosef El-manawy <yusuf.hussien42@gmail.com>
2025-04-15 14:54:50 +0200

    fixing no session error

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -99,21 +99,25 @@ export const verifySecret = async ({
 };
 
 export const getCurrentUser = async () => {
-  const { databases, account } = await createSessionClient();
+  try {
+    const { databases, account } = await createSessionClient();
 
-  const result = await account.get();
+    const result = await account.get();
 
-  const user = await databases.listDocuments(
-    appwriteConfig.databaseId,
-    appwriteConfig.usersCollectionId,
-    [Query.equal("accountId", [result.$id])]
-  );
+    const user = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      [Query.equal("accountId", [result.$id])]
+    );
 
-  if (user.total <= 0) {
-    return null;
+    if (user.total <= 0) {
+      return null;
+    }
+
+    return parseStringify(user.documents[0]);
+  } catch (error) {
+    console.log(error);
   }
-
-  return parseStringify(user.documents[0]);
 };
 
 export const signOutUser = async () => {
